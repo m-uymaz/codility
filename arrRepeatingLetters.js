4635483458158b1ac0477617dc3f153274f86b81
Mihail Uymaz <michael.m.uymaz@gmail.com>
2021-10-26 17:01:17 +0300

    repeating letters in a string

---
+++ arrRepeatingLetters.js
@@ -0,0 +1,18 @@
+// Check if a STRING has REAPEATING letters in it!
+// 100% solution in Codility
+
+// The first index is compared against the other Indices to find a match.
+// If it finds -> returns the match.
+function solution(S) {
+    const split = S.split('')
+    for (let i = 0; i < split.length; i++) {
+        for (let j = i + 1; j < split.length; j++) {
+            if (split[i] === split[j]) {
+                return split[i];
+            }
+        }
+    }
+    return 'No match found!'
+}
+
+//WARNING: Be sure to check and test the code!!!
