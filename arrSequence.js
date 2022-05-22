051826c6fc8690162aa4677911c4c4f1b9ca5b39
m-uymaz <51107546+m-uymaz@users.noreply.github.com>
2022-05-22 06:35:19 +0300

    Update arrSequence.js

--- arrSequence.js
+++ arrSequence.js
@@ -2,8 +2,8 @@
 // CHECK THE SEQUENCE OF AN ARR (A) FROM 1 TO (K)
 
 function solution(A, K) {
-    var n = A.length;
-    for (var i = 0; i < n - 1; i++) {
+    let n = A.length;
+    for (let i = 0; i < n - 1; i++) {
         if (A[0] !== 1 || A[n - 1] !== K)
             return false; 
         if (A[i] !== A[i + 1] && A[i] + 1 !== A[i + 1])
